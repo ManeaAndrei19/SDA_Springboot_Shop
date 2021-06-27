9bf813fa07f7611a007e8ad64adbc4485f3c2ed0
ManeaAndrei19 <maneaandrei19@yahoo.com>
2021-06-27 09:02:54 +0300

    Add Product Category Label

--- src/main/java/com/sda/andreimanea/webshop/model/ProductCategory.java
+++ src/main/java/com/sda/andreimanea/webshop/model/ProductCategory.java
@@ -1,9 +1,16 @@
 package com.sda.andreimanea.webshop.model;
 
 public enum ProductCategory {
-    GROCERY,
-    PET_FOOD,
-    ELECTRONICS,
-    MISCELLANEOUS
-
+    GROCERY("Grocery"),
+    PET_FOOD("Pet food"),
+    ELECTRONICS("Electronics"),
+    MISCELLANEOUS("Miscellaneous"),
+    BEVERAGES("Beverages");
+    private final String displayValue;
+    ProductCategory(String displayValue) {
+        this.displayValue = displayValue;
+    }
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
