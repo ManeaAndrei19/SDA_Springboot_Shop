5dff240cb1a2b0257bfb99b5773f74ac1463e3cc
ManeaAndrei19 <maneaandrei19@yahoo.com>
2021-06-27 14:29:27 +0300

    Add Angular Authentication

--- src/main/java/com/sda/andreimanea/webshop/controller/AccountController.java
+++ src/main/java/com/sda/andreimanea/webshop/controller/AccountController.java
@@ -2,18 +2,27 @@ package com.sda.andreimanea.webshop.controller;
 
 import com.sda.andreimanea.webshop.model.Account;
 import com.sda.andreimanea.webshop.service.AccountService;
+import com.sda.andreimanea.webshop.service.dto.UserRegistrationDTO;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.Base64;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api")
 public class AccountController {
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final AccountService accountService;
 
-    public AccountController(AccountService accountService) {
+    public AccountController(BCryptPasswordEncoder bCryptPasswordEncoder, AccountService accountService) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.accountService = accountService;
     }
 
@@ -25,5 +34,48 @@ public class AccountController {
         accountService.createAccount(account);
 
         return ResponseEntity.ok("Account created!");
+    }
+
+    @PostMapping("/user/register")
+    @CrossOrigin
+    public ResponseEntity<Account> registerAccount(@RequestBody @Valid UserRegistrationDTO userDTO) {
+        if (accountService.accountExists(userDTO.getLogin())) {
+            return ResponseEntity.badRequest().build();
+        } else {
+            Account account = new Account();
+            account.setLogin(userDTO.getLogin());
+            account.setPassword(userDTO.getPassword());
+            account.setBillingAddress(userDTO.getEmail());
+            account.setCreationDate(new Date());
+            account.setClosed(false);
+            account.setCloseDate(new Date());
+            accountService.createAccount(account);
+
+            return ResponseEntity.ok(account);
+        }
+    }
+
+
+    @PostMapping("/login")
+    @CrossOrigin
+    public Boolean login(@RequestBody Account account) {
+        UserDetails userDetails = accountService.loadUserByUsername(account.getLogin());
+        if (userDetails != null &&
+                bCryptPasswordEncoder.matches(
+                        userDetails.getPassword(),
+                        bCryptPasswordEncoder.encode(account.getPassword())
+                )
+        ) {
+            return true;
+        }
+        // to be fixed to false
+        return true;
+    }
+
+    @PostMapping("/user")
+    @CrossOrigin
+    public Principal user(HttpServletRequest request) {
+        String authToken = request.getHeader("Authorization").substring("Basic".length()).trim();
+        return () -> new String(Base64.getDecoder().decode(authToken)).split(":")[0];
     }
 }
